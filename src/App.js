ed049fc26fc1f667a364ddf7401fb618ba37d35e
Sefa Kose <sfkse5591@gmail.com>
2021-09-20 21:13:42 +0200

    Integrate  json server

--- src/App.js
+++ src/App.js
@@ -1,34 +1,20 @@
 import './App.css';
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Header from './components/Header';
 import Tasks from './components/Tasks';
 import AddTask from './components/AddTask';
 
 function App() {
-  const [tasks, setTask] = useState([
-
-    {
-      "id": 1,
-      "text": "Study React Pre-Class Notes",
-      "day": "Feb 5th at 2:30pm",
-      "isDone": false
-    },
-    {
-      "id": 2,
-      "text": "Feed the Dog",
-      "day": "Feb 6th at 1:30pm",
-      "isDone": false
-    },
-    {
-      "text": "Wash car",
-      "day": "Feb 10th at 3:00pm",
-      "isDone": false,
-      "id": 3
-    }
-
-  ]);
+  const [tasks, setTask] = useState([]);
   const [visible, setVisible] = useState(false);
   // console.log(tasks)
+  const baseUrl = " http://localhost:3001/tasks";
+
+  useEffect(() => {
+    fetch(baseUrl)
+      .then(response => response.json())
+      .then(data => setTask(data))
+  }, [])
 
   const handleVisible = () => {
 
